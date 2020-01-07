fb349ee0d0ff1644ec816114e8b6baa0f3f7f47f
czyzu05 <damian.czyz1998@gmail.com>
2020-01-07 15:16:23 +0100

    Added first game first level

--- SkillerGame/SkillerGame/View/FirstLevel.xaml.cs
+++ SkillerGame/SkillerGame/View/FirstLevel.xaml.cs
@@ -23,6 +23,66 @@ namespace SkillerGame
         public FirstLevel()
         {
             InitializeComponent();
+            MessageBox.Show("Znajdź i kliknij na liczbę 1");
         }
+
+        public void StartTimer(object o, RoutedEventArgs sender)
+        {
+            System.Windows.Threading.DispatcherTimer myDispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            myDispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 1000);
+            myDispatcherTimer.Tick += new EventHandler(Each_Tick);
+            myDispatcherTimer.Start();
+        }
+
+        // A variable to count.
+        int i = 0;
+
+        // Fires every 1000 miliseconds while the DispatcherTimer is active.
+        public void Each_Tick(object o, EventArgs sender)
+        {
+            time.Text = "Count up: " + i++.ToString();
+            if (i > 15) outOfTime();
+        }
+
+        private void generateNumbersWithOneOther(object sender, RoutedEventArgs e)
+        {
+            int position = Convert.ToInt32(((Button)sender).Tag);
+            Button newbtn = new Button();
+            Random rnd = new Random();
+            int number = rnd.Next(1, 85);
+            if (position == number)
+                newbtn.Content = 1;
+            else
+                newbtn.Content = 7;
+
+            ((Button)sender).Content = newbtn.Content;
+
+        }
+
+        private void checkNumber(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            int choose = Convert.ToInt32((btn.Content));
+            if (choose == 1)
+            {
+                MessageBox.Show("Wygrałeś");
+                System.Diagnostics.Process.GetCurrentProcess().Kill();
+            }
+            else
+                wrongNumber();
+        }
+
+        private void wrongNumber()
+        {
+            MessageBox.Show("Zła liczba, przegrałeś :(");
+            System.Diagnostics.Process.GetCurrentProcess().Kill();
+        }
+
+        private void outOfTime()
+        {
+            MessageBox.Show("Upłynął czas, przegrałeś :(");
+            System.Diagnostics.Process.GetCurrentProcess().Kill();
+        }
+
     }
 }
